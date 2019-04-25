b0437c6057f832679ec0e8985139c88b68ecbe6b
SeverePain <47048628+SeverePain@users.noreply.github.com>
2019-04-25 16:18:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = process.env.ENDtoken
+let tokenfile = process.env.ENDtoken
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
