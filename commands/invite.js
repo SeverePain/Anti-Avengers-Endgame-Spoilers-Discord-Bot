f9e7447e23b54cf34d9b1cf7022ca9fedd812700
SeverePain <47048628+SeverePain@users.noreply.github.com>
2019-04-25 17:01:01 -0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     message.author.send("https://discordapp.com/oauth2/authorize?client_id=571104579903815691&permissions=482470983&scope=bot is the invite link! Make sure you have Dyno and have already created the 'Muted' role. All events are logged in #logs! The bot role should be put above the Muted role for it to work!").catch(err => {
-      message.channel.send("I cannot DM you!")
+      message.reply("I cannot DM you!")
       console.log(err)
       return;
     })
