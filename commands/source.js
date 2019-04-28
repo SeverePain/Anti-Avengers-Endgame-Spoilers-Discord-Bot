adf491ddf1fc897c739896df19cf468a91ecbd6e
SeverePain <47048628+SeverePain@users.noreply.github.com>
2019-04-27 17:38:12 -0700

    a

--- commands/source.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.reply("The Source Code for the bot is open source and on github! https://github.com/SeverePain/Anti-Avengers-Endgame-Spoilers-Discord-Bot")
-    }
- 
-    module.exports.help = {
-      name: "source"
-    }
